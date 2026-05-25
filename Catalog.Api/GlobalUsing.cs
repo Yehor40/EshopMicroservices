0ea184b45fff89632f8396f4cba905a6bfe1d818
Yehor40 <fraugrey3@gmail.com>
2026-05-26 00:04:15 +0200

    [Feat]:Added validation behavior for create command

--- Catalog.Api/GlobalUsing.cs
+++ Catalog.Api/GlobalUsing.cs
@@ -5,3 +5,4 @@ global using Catalogue.Api.Models;
 global using ClassLibrary1.CQRS;
 global using Marten;
 global using Catalogue.Api.Exceptions;
+global using FluentValidation;
--- Catalog.Api/Program.cs
+++ Catalog.Api/Program.cs
@@ -1,7 +1,15 @@
+using ClassLibrary1.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 //DI container
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddCarter();
-builder.Services.AddMediatR(config => { config.RegisterServicesFromAssembly(typeof(Program).Assembly); });
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
