80a7b933d3729bf23e698c95962fe49ebe89b915
Yehor40 <fraugrey3@gmail.com>
2026-06-01 23:17:02 +0200

    [Feat]:new exceptions added

---
+++ BuildingBlocks/Exceptions/BadRequestException.cs
@@ -0,0 +1,14 @@
+namespace ClassLibrary1.Exceptions;
+
+public class BadRequestException:Exception
+{
+    public BadRequestException(string message) : base(message)
+    {
+    }
+    
+    public BadRequestException(string message,string details) : base(message)
+    {
+        Details = details;
+    }
+    public string? Details { get; }
+}
