f24051b8e5df7566bdf87265e84537571bbaa51e
Yehor40 <fraugrey3@gmail.com>
2026-06-06 23:51:38 +0200

    [Init]:Basket service added

---
+++ Basket.Api/Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+namespace Basket.Api.Models;
+
+public class ShoppingCart
+{
+    public string UserName { get; set; }
+    public List<ShoppingCartItem> Items { get; set; } = new();
+    
+    public decimal TotalPrice=> Items.Sum(i => i.Price*i.Quantity);
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+    //for mapping 
+    public ShoppingCart()
+    {
+    }
+}
