8e5ad3133c36d12e32af624eefe1abf0a511b57d
Yehor40 <fraugrey3@gmail.com>
2026-06-07 23:59:46 +0200

    [Feat]:post endpoint added

--- Basket.Api/Basket/StoreBasket/StoreBasketEndpoints.cs
+++ Basket.Api/Basket/StoreBasket/StoreBasketEndpoints.cs
@@ -1,6 +1,21 @@
 namespace Basket.Api.Basket.StoreBasket;
-
-public class StoreBasketEndpoints
+public record StoreBasketRequest(ShoppingCart Cart);
+public record StoreBasketResponse(string UsernName);
+public class StoreBasketEndpoints:ICarterModule
 {
-    
-}
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<StoreBasketCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<StoreBasketResponse>();
+            return Results.Created($"/basket/{result.UsernName}", response);
+        })
+        .WithName("StoreBasket")
+        .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Store basket")
+        .WithDescription("Store basket");
+    }
+} 
