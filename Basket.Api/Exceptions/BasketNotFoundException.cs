886cd9a8aa7bf03f52edcafeefc4890daa256296
Yehor40 <fraugrey3@gmail.com>
2026-06-08 21:15:44 +0200

    [Feat]:repository-added

---
+++ Basket.Api/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,8 @@
+namespace Basket.Api.Exceptions;
+
+public class BasketNotFoundException:NotFoundException
+{
+    public BasketNotFoundException(string userName) : base("Basket",userName)
+    {
+    }
+}
--- Basket.Api/GlobalUsing.cs
+++ Basket.Api/GlobalUsing.cs
@@ -5,4 +5,8 @@ global using MediatR;
 global using Mapster;
 global using FluentValidation;
 global using ClassLibrary1.Behaviors;
+global using ClassLibrary1.Exceptions;
+global using Marten;
+global using Basket.Api.Exceptions;
+
 
