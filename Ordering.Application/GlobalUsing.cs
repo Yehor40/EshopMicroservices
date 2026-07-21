3a3a5a4b40685da19a64381f768b07694a980b0f
Yehor40 <fraugrey3@gmail.com>
2026-07-22 01:07:43 +0200

    [Feat]:create-order-command-added

---
+++ Ordering.Application/GlobalUsing.cs
@@ -0,0 +1,5 @@
+global using ClassLibrary1.CQRS;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
---
+++ Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,18 @@
+using ClassLibrary1.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto Order):ICommand<CreateOrderResult>;
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(x=>x.Order.OrderName).NotEmpty().WithMessage("Name is required.");
+        RuleFor(x=>x.Order.CustomerId).NotNull().WithMessage("Customer Id is required.");
+        RuleFor(x=>x.Order.OrderItems).NotEmpty().WithMessage("Order Items shoud not be empty.");
+    }
+}
