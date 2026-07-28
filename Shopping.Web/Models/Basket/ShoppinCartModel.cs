b5ab21cdb6a965c1c6972b79d3bc7a59ff4ed3a1
Yehor40 <fraugrey3@gmail.com>
2026-07-28 17:46:25 +0200

    [Feat]:razor-pages-added

---
+++ Shopping.Web/Models/Basket/ShoppinCartModel.cs
@@ -0,0 +1,24 @@
+using Shopping.Web.Models.Catalog;
+
+namespace Shopping.Web.Models.Basket;
+
+public class ShoppinCartModel
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItemModel> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
+
+public class ShoppingCartItemModel
+{
+    public int Quantity { get; set; } = default!;
+    public string Color { get; set; } = default!;
+    public decimal Price { get; set; } = default!;
+    public Guid ProductId { get; set; } = default!;
+    public string ProductName { get; set; } = default!;
+}
+//wrapper classes
+public record GetBasketResponse(ShoppinCartModel Cart);
+public record StoreBasketRequest(ShoppinCartModel Cart);
+public record StoreBasketResponse(string UserName);
+public record DeleteBasketRequest(bool IsSuccess);
